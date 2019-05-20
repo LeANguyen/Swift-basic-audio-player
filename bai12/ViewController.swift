b1fe436f08abef8584a589c8fbd7e5242db11d37
mac <mac@pc.rmit.edu.vn>
2019-05-20 17:23:07 +0700

    Initial Commit

---
+++ bai12/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bai12
+//
+//  Created by mac on 5/20/19.
+//  Copyright © 2019 Le An Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
